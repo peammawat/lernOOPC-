d1960aa7033a476e0f0c6577fc2954abcb835a87
peammawat <peammawat.thongpat@gmail.com>
2025-02-23 19:21:37 +0700

    new

---
+++ Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lernOOPC_
+{
+    internal class Employee
+    {
+
+        private string name;
+        private int salary;
+
+        public Employee() {
+            Console.WriteLine("Create object Employee Class");
+        }
+
+        public Employee(string nameValue, int salaryValue) {
+            this.name = nameValue;
+            this.salary = salaryValue;
+        }
+
+        /*public void showDetail() {
+            Console.WriteLine("Name = " + this.name);
+            Console.WriteLine("Salary = " + this.salary);
+        }
+
+        public void setName(string newName)
+        {
+            this.name = newName;
+        }
+
+        public void setSalary(int newSalary)
+        {
+            this.salary = newSalary;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public int getSalary()
+        {
+            return this.salary;
+        }*/
+        
+        /*public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }*/
+
+        /*public string Name { get; set; }
+        public int Salary { get; set; }*/
+
+        public string Name { get => name; set => name = value; }
+        public int Salary { get => salary; set => salary = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lernOOPC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            Employee emp2 = new Employee("peam",50000);
+
+            /*emp2.showDetail();
+             * 
+            emp2.setName("peammawat");
+            emp2.setSalary(100000);
+
+            Console.WriteLine(emp2.getName());
+            Console.WriteLine(emp2.getSalary());
+
+            emp2.showDetail();*/
+
+            /*Console.WriteLine(emp2.Name);
+            Console.WriteLine(emp2.Salary);*/
+            emp2.Name = "peammawat";
+            emp2.Salary = 100000;
+            Console.WriteLine(emp2.Name);
+            Console.WriteLine(emp2.Salary);
+
+            Console.ReadKey();
+        }
+    }
+}
